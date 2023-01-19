69ada4032785de2ed6c8bc508794b6fd65d169f3
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-01-18 22:52:17 -0300

    isolando arquivos do backend

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+
+import { FastifyInstance } from "fastify"
+import { prisma } from "./lib/prisma"
+
+export async function appRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+     where: {
+       title: {
+         startsWith: 'Beber'
+       }
+     }
+    })
+  
+    return habits
+  })
+}
+
